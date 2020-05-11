b31ffbc8fff51ae02f8e2294f0f10f1037ca76b8
Wenderson <wps713@gmail.com>
2020-05-11 10:44:17 -0300

    Add appointments post route

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -5,6 +5,16 @@ const appointmentsRouter = Router();
 
 const appointments = [];
 
+appointmentsRouter.post('/', (request,response) => {
+  const {provider, date} = request.body;
+  const appointment = {
+    id : uuid(),
+    provider,
+    date
+  }
+  appointments.push(appointment)
+  return response.json(appointment)
+})
 
 export default appointmentsRouter;
 
