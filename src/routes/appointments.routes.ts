36ae968f2c7ba61d543a63e8708e6458a1a389e0
Wenderson <wps713@gmail.com>
2020-05-20 12:16:29 -0300

    Settingup ensure Authenticated middleware in route

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -3,8 +3,11 @@ import { parseISO } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
+import ensureAuthentication from '../middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
+
+appointmentsRouter.use(ensureAuthentication);
 
 appointmentsRouter.get('/', async (request, response) => {
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
